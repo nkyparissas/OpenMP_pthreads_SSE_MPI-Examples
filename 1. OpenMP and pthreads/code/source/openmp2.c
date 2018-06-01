3ef42a7db19af332f36595e48113ba7558789862
Nick Kyparissas <nkyparissas@users.noreply.github.com>
2018-06-01 05:02:20 +0300

    Update openmp2.c

--- 1. OpenMP and pthreads/code/source/openmp2.c
+++ 1. OpenMP and pthreads/code/source/openmp2.c
@@ -71,7 +71,7 @@ double finky_freeky(int m, int n, int l, int cores, int seed, int loop){
 
 	for (int i = 0; i < l; i++ )
 		for(int j = 0; j < n; j++)
-			B[i][j] =(rand()%2 == 0)?'0':'1' ;
+			B[i][j] = (rand()%2 == 0)?'0':'1' ;
 
 	int** HamTable;
 
@@ -140,8 +140,8 @@ double finky_freeky(int m, int n, int l, int cores, int seed, int loop){
 	*/
 	unsigned long long int sum = 0;
 	for (int i = 0; i < m; i++)
-			for(int j = 0; j < n; j++)
-				sum = HamTable[i][j] + sum;
+		for(int j = 0; j < n; j++)
+			sum = HamTable[i][j] + sum;
 	
 	/*
 	Print the results if this is the last loop executed.
@@ -153,7 +153,7 @@ double finky_freeky(int m, int n, int l, int cores, int seed, int loop){
 		free (HamTable[i]);
 	free (HamTable);
 	
-	for (int i = 0; i < l; i++)	{
+	for (int i = 0; i < l; i++){
 		free(A[i]);
 		free(B[i]);
 	}
